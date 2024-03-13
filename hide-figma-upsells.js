cc3149bddad5228c9cd198c40a94609d727d94d5
MoonLyss <shaitan.lyssm@gmail.com>
2024-03-13 20:46:20 +0100

    Fix manifest

--- hide-figma-upsells.js
+++ hide-figma-upsells.js
@@ -1,6 +1,16 @@
-console.log("Hello my boi")
+
 const upsellElmnts = document.querySelectorAll('[class*="upsell"]');
-console.log(upsellElmnts)
-for (const e of upsellElmnts) {
-  e.remove()
-}
+// Creating an observer instance
+const observer = new MutationObserver((mutations) => {
+  mutations.forEach((mutation) => {
+    if (mutation.addedNodes.length) {
+      mutation.addedNodes.forEach((n) => {
+        console.log("Removing", n)
+        n.remove()
+      })
+    }
+  });
+});
+// Configuration of the observer:
+const config = { childList: true, subtree: true };
+observer.observe(document.body, config)
