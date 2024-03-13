766c2a9adbae2b6ec544fad88e7aec4a73c08dd2
MoonLyss <shaitan.lyssm@gmail.com>
2024-03-13 21:21:34 +0100

    Fix manifest

--- hide-figma-upsells.js
+++ hide-figma-upsells.js
@@ -4,11 +4,12 @@ const observer = new MutationObserver((mutations) => {
   mutations.forEach((mutation) => {
     if (mutation.addedNodes.length) {
       mutation.addedNodes.forEach((n) => {
-        console.log(n)
         if (n instanceof Element) {
           const upsellElmnts = n.querySelectorAll('[class*="upsell"], [data-onboarding-key="file_tracker_upsell_tooltip_key"]');
-          for (const e of upsellElmnts)
+          for (const e of upsellElmnts) {
+            console.log("Removing upsell", e)
              e.remove() 
+          }
         }
       })
     }
