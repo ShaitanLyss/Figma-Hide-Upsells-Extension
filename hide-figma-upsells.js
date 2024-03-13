a85ad3e2219df60e5f1138ae6d31798bc0f753ac
MoonLyss <shaitan.lyssm@gmail.com>
2024-03-13 20:39:59 +0100

    Fix manifest

--- hide-figma-upsells.js
+++ hide-figma-upsells.js
@@ -1,6 +1,6 @@
 console.log("Hello my boi")
 const upsellElmnts = document.querySelectorAll('[class*="upsell"]');
-console.log("upsellElmnts")
+console.log(upsellElmnts)
 for (const e of upsellElmnts) {
   e.remove()
 }
